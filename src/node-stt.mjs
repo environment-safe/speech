f3de3eddaf8b5ff52fbdd7b1400e381f94e9bcee
khrome <abbey@khrome.net>
2025-02-13 14:19:38 -0700

    no whisper defaults

--- src/node-stt.mjs
+++ src/node-stt.mjs
@@ -1,4 +1,5 @@
-/*import { play, recordUserSpeech } from './util.mjs';
+//*
+//import { play, recordUserSpeech } from './util.mjs';
 
 
 export const ready = new Promise((resolve)=>{
@@ -6,10 +7,12 @@ export const ready = new Promise((resolve)=>{
 });
 
 export const hear = async (options={})=>{
-    const audio = await recordUserSpeech(options);
-    play(audio, { sampleRate: 8000 });
-};*/
+    //const audio = await recordUserSpeech(options);
+    //play(audio, { sampleRate: 8000 });
+}; //*/
 // until node has a native module that can locally stt, 
 // we use whisper by default at the expense of speed
+/*
 import { ready, hear } from './whisper-stt.mjs';
 export { ready, hear };
+//*/
